639f39c6d587e1fc6af627f859b7de50450d613c
Gabriel <gabrielmuhoro@gmail.com>
2023-08-09 09:48:38 +0300

    complete

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantsController < ApplicationController
+    def index 
+        restaurants = Restaurant.all 
+        render json: restaurants
+       end
+       
+       def show 
+         restaurant = Restaurant.find(params[:id]) 
+         render json: restaurant , include: :pizzas
+       end
+   
+       def destroy
+           restaurant = Restaurant.find(params[:id])
+           if restaurant
+             restaurant.destroy
+             head :no_content
+           else
+             render json: { error: "Restaurant not found" }, status: :not_found
+           end
+       end
+      
+end
